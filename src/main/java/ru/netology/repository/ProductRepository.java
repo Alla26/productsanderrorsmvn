57f80ddc8452b3612faea7b36a7c5697281373ed
unknown <girlalla@yandex.ru>
2022-07-30 15:56:52 +0300

    Создала класс исключения NotFoundException, модернизировала метод removeById, дописала тесты

---
+++ src/main/java/ru/netology/repository/ProductRepository.java
@@ -0,0 +1,118 @@
+package ru.netology.repository;
+
+import ru.netology.repository.NotFoundException;
+import ru.netology.domain.Product;
+
+public class ProductRepository {
+
+    private Product[] products = new Product[0];
+
+    public Product[] getProducts() {
+        return products;
+    }
+
+    public void save(Product product) {                      //сохраняет продукт
+        Product[] tmp = new Product[products.length + 1];
+        for (int i = 0; i < products.length; i++) {
+            tmp[i] = products[i];
+        }
+        tmp[tmp.length - 1] = product;
+        products = tmp;
+    }
+
+    public Product[] findAll() {                          //возвращает все продукты
+        Product[] all = getProducts();
+        Product[] list = new Product[all.length];
+        for (int i = 0; i < list.length; i++) {
+            list[i] = all[i];
+        }
+        return list;
+    }
+
+    public Product[] removeById(int id) {//удаляет продукты по ID
+        /* String result = null;*/
+        /* findById(int id);*/
+        if (findById(id) == null) {
+            throw new NotFoundException(
+                    "Товар с указанным ID не найден: " + id
+            );
+        }
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+        return tmp;
+    }
+
+
+    public Product[] findById(int id) {             //исходник файнд
+        Product[] tmp = new Product[1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() == id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+
+                // products = tmp;
+                return products;
+            }
+        }
+        return null;
+    }
+
+    /* public Product[] findById(int id) {
+        Product[] tmp = new Product[products.length];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+
+              /*  products = tmp;*/
+    //        return products;
+    //         }
+    //   }
+    //     return null;
+    //   }*/
+
+
+   /* public Product[] removeById(int id) {                    // исходник удаляет продукты по ID
+        Product[] tmp = new Product[products.length - 1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() != id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+            }
+        }
+        products = tmp;
+        return tmp;
+    }*/
+
+
+   /* public Product[] findByDyId(int id) {
+        Product[] tmp = new Product[1];
+        int copyToIndex = 0;
+        for (Product product : products) {
+            if (product.getId() == id) {
+                tmp[copyToIndex] = product;
+                copyToIndex++;
+
+                products = tmp;
+                return products;
+            } /*else {
+                products = null;
+                return products;
+            }*/
+    //    }
+    /* return;*/
+    //      return null /*products*/;
+    // }
+
+
+}
